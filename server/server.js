af61b77fa9c42b8c442d065bb6818819451c7368
pagigon <y.k.hedgehog@icloud.com>
2025-07-29 00:20:41 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,35 +8,11 @@ const io = require('socket.io')(http, {
     }
 });
 const path = require('path');
+
 const { setupSocketHandlers } = require('./socketHandlers');
 
 // 静的ファイルの配信
 app.use(express.static(path.join(__dirname, '../public')));
-
-// デバッグ用ルート（追加）
-app.get('/debug', (req, res) => {
-    const fs = require('fs');
-    const publicPath = path.join(__dirname, '../public');
-    const cssPath = path.join(publicPath, 'css');
-    const jsPath = path.join(publicPath, 'js');
-    
-    try {
-        const publicFiles = fs.readdirSync(publicPath);
-        const cssFiles = fs.existsSync(cssPath) ? fs.readdirSync(cssPath) : ['CSS folder not found'];
-        const jsFiles = fs.existsSync(jsPath) ? fs.readdirSync(jsPath) : ['JS folder not found'];
-        
-        res.json({
-            publicPath,
-            publicFiles,
-            cssFiles,
-            jsFiles,
-            workingDirectory: process.cwd()
-        });
-    } catch (error) {
-        res.json({ error: error.message });
-    }
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
